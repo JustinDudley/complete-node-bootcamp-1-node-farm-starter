dc6d746320479b7de969ac1164d816f399ee1c8c
JustinDudley <justindudley99@gmail.com>
2022-04-04 11:55:38 -0400

    add asynchronous readFile, and writeFile

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const fs = require("fs");
 
-// synchronous reading of file
+// blockig, synchronous reading of file
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
-
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written !");
+
+// non-blocking, asynchronous reading, basic
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => console.log(data));
+// console.log("will read file...");
+
+// non-blocking, asynchronous reading, more advanced  (BUT uses callback hell)
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  //   if (err) {return console.log("error reading first file ");}
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) =>
+        console.log("Your file has been written")
+      );
+    });
+  });
+});
