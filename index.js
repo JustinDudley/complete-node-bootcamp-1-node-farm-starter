619388d80854321b9f8682a2d2c36950f1cbedb0
JustinDudley <justindudley99@gmail.com>
2022-04-05 14:22:16 -0400

    write header of type application/json

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ const server = http.createServer((req, res) => {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       console.log(productData);
-      // send the original JSON string
+      res.writeHead(200, { "Content-type": "application/json" });
+      // send the original JSON string, not the js object
       res.end(data);
       //   res.end("API");
     });
