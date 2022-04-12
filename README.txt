706ada62bf14f8441d8108c546e4296dba885d77
JustinDudley <justindudley99@gmail.com>
2022-04-12 12:08:33 -0400

    define injectTemplate before exporting it (not really necessary), and add a README

---
+++ README.txt
@@ -0,0 +1,23 @@
+This project was built by following the 
+Udemy class with 
+Jonas Schmedtmann.
+
+NodeJS Express Mongodb Bootcamp
+https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/
+Section 2, chapters 4 - 17
+
+The meat of the index.js is the section under the comment: //SERVER
+
+There is also, first a section under the comment: //FILES
+This "files" section causes an existing file to be updated 
+with Date.now() every
+time you run the project. Don't be alarmed if your git changes
+keep showing that a file has been changed after you run the project.
+
+Also, there is a shadow index file called index-with-comments.js
+This file is not called by anything, anywhere.
+It has a bunch of useful comments from stuff Jonas said.
+It was copy-pasted near the end of the project.  It may not 
+quite be functional. In particular, the function called injectTemplate
+is still included in the file; it has not yet been abstracted to its
+own module.
--- modules/injectTemplate.js
+++ modules/injectTemplate.js
@@ -1,7 +1,7 @@
 // we have access to the "module" object in every node module
 // we create, including this one
 
-module.exports = (template, product) => {
+const injectTemplate = (template, product) => {
   let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
@@ -17,8 +17,24 @@ module.exports = (template, product) => {
   return output;
 };
 
-// This function used to look like this...
-// const injectTemplate = (template, product) => {
-//     let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-//     ...
-//     ...
+module.exports = injectTemplate;
+
+//
+// or, shorthand, without defining AND THEN exporting our function, we
+// could replace ALL of the above code with this:
+
+// module.exports = (template, product) => {
+//   let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//   output = output.replace(/{%PRICE%}/g, product.price);
+//   output = output.replace(/{%FROM%}/g, product.from);
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//   output = output.replace(/{%ID%}/g, product.id);
+
+//   if (!product.organic) {
+//     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+//   }
+//   return output;
+// };
